9d6e0cedd25a320abe4425a9959759ab3605f7aa
RodrigoC3805 <rodrigocastillo3805@gmail.com>
2025-05-03 22:04:59 -0500

    created Springboot project
    added Dockerfile, .env, application.properties

---
+++ src/test/java/dsw/backendSiderandina/BackendSiderandinaApplicationTests.java
@@ -0,0 +1,13 @@
+package dsw.backendSiderandina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSiderandinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
