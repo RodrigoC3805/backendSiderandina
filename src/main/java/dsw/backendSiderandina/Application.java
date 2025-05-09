8b9c77a3eb9673220af92a088ea1b543c6f315b6
RodrigoC3805 <rodrigocastillo3805@gmail.com>
2025-05-08 21:12:12 -0500

    added CORS

--- src/main/java/dsw/backendSiderandina/Application.java
+++ src/main/java/dsw/backendSiderandina/Application.java
@@ -2,6 +2,8 @@ package dsw.backendSiderandina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +11,12 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
+	}
 }
