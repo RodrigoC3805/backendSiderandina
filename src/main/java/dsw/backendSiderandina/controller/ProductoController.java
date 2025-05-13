4afd00bbdc5ba3524d331b5812bfbdea50f9ee29
nxn24 <nisavi.18@gmail.com>
2025-05-13 14:06:22 -0500

    added buscarProductoPorNombre in ProductoController, ProductoRepository and ProductoService

--- src/main/java/dsw/backendSiderandina/controller/ProductoController.java
+++ src/main/java/dsw/backendSiderandina/controller/ProductoController.java
@@ -7,10 +7,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import dsw.backendSiderandina.dto.ProductoRequest;
 import dsw.backendSiderandina.dto.ProductoResponse;
 import dsw.backendSiderandina.service.ProductoService;
 import dsw.backendSiderandina.utils.ErrorResponse;
@@ -36,4 +42,86 @@ public class ProductoController {
                     .body(ErrorResponse.builder().message("Producto not found"));
         return ResponseEntity.ok(listaProductoResponse);
     }
+
+    @GetMapping("/find")
+    public ResponseEntity<?> findProductoById(@RequestBody ProductoRequest productoRequest) {
+        logger.info(">find " + productoRequest.toString());
+        ProductoResponse productoResponse;
+        try {
+            productoResponse=productoService.findProducto(productoRequest.getIdProducto());
+        } catch (Exception e) {
+            logger.error("Error inesperado", e);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if (productoResponse==null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(ErrorResponse.builder().message("Producto not found"));
+        return ResponseEntity.ok(productoResponse);
+    }
+
+    @GetMapping("/buscar")
+public ResponseEntity<?> buscarPorNombre(@RequestParam String nombre) {
+    List<ProductoResponse> productos;
+    try {
+        productos = productoService.buscarPorNombre(nombre);
+    } catch (Exception e) {
+        logger.error("Error inesperado", e);
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    if (productos == null || productos.isEmpty())
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponse.builder().message("Producto not found"));
+        return ResponseEntity.ok(productos);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> insertProducto(@RequestBody ProductoRequest productoRequest) {
+        logger.info(">insert " + productoRequest.toString());
+        ProductoResponse productoResponse;
+        try {
+            productoResponse = productoService.insertProducto(productoRequest);
+        } catch (Exception e) {
+            logger.error("Error inesperado", e);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if (productoResponse==null) 
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(ErrorResponse.builder().message("Producto not found"));
+        return ResponseEntity.ok(productoResponse);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateProducto(@RequestBody ProductoRequest productoRequest) {
+        logger.info(">update " + productoRequest.toString());
+        ProductoResponse productoResponse;
+        try {
+            productoResponse = productoService.updateProducto(productoRequest);
+        } catch (Exception e) {
+            logger.error("Error inesperado", e);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if (productoResponse == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(ErrorResponse.builder().message("Producto not found"));
+        return ResponseEntity.ok(productoResponse);
+    }
+
+     @DeleteMapping
+    public ResponseEntity<?> deleteProducto(@RequestBody ProductoRequest productoRequest) {
+        logger.info(">delete " + productoRequest.toString());
+        ProductoResponse productoResponse;
+        try {
+            productoResponse=productoService.findProducto(productoRequest.getIdProducto());
+            if (productoResponse==null)
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.builder().message("Producto not found for deletion"));
+            productoService.deleteProducto(productoResponse.getIdProducto());
+        } catch (Exception e) {
+            logger.error("Error inesperado", e);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return ResponseEntity.ok(productoResponse);
+    }
+
+    
+
 }
--- src/main/java/dsw/backendSiderandina/service/ProductoService.java
+++ src/main/java/dsw/backendSiderandina/service/ProductoService.java
@@ -1,6 +1,7 @@
 package dsw.backendSiderandina.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,4 +35,38 @@ public class ProductoService {
         producto = productoRepository.save(producto);
         return ProductoResponse.fromEntity(producto);
     }
+
+    public ProductoResponse updateProducto(ProductoRequest productoRequest) {
+        Producto producto = new Producto(
+            productoRequest.getIdProducto(),
+            productoRequest.getSku(),
+            productoRequest.getNombre(),
+            productoRequest.getPrecioVentaBase(),
+            productoRequest.getCostoUnitarioBase(),
+            productoRequest.getStock(),
+            productoRequest.getStockMin(),
+            productoRequest.getUrlImagen(),
+            null, // Assuming CategoriaProducto and UnidadesMedida are set later
+            null
+        );
+        producto = productoRepository.save(producto);
+        return ProductoResponse.fromEntity(producto);
+    }
+
+    public void deleteProducto(Integer id) {
+        productoRepository.deleteById(id);
+    }
+
+    public ProductoResponse findProducto(Integer id) {
+        Optional<Producto> result = productoRepository.findById(id);
+        if (!result.isPresent())
+            return null;
+        return ProductoResponse.fromEntity(result.get());
+    }
+    
+    // metodo para buscar productos por nombre
+    public List<ProductoResponse> buscarPorNombre(String nombre) {
+        return ProductoResponse.fromEntities(productoRepository.findByNombreContainingIgnoreCase(nombre));
+    }
+
 }
