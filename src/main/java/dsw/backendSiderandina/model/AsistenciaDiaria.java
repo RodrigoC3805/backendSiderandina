346baec3d3d193d7acbf41d162a7ceeeba78dd4f
nxn24 <nisavi.18@gmail.com>
2025-06-04 16:25:12 -0500

    modified DB references in model AsistenciaDiaria

--- src/main/java/dsw/backendSiderandina/model/AsistenciaDiaria.java
+++ src/main/java/dsw/backendSiderandina/model/AsistenciaDiaria.java
@@ -14,13 +14,17 @@ import java.time.LocalTime;
 public class AsistenciaDiaria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_asistencia_diaria")
     private Integer idAsistencia;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "id_trabajador", referencedColumnName = "id_trabajador")
     private Trabajador trabajador;
-
+    
+    @Column(name="dia_asistencia")
     private LocalDate fecha;
+    @Column(name="hora_entrada")
     private LocalTime horaIngreso;
+    @Column(name="hora_salida")
     private LocalTime horaSalida;
 }
